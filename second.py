39dafecf015a8b2fa05bdecae0c011bb5780e672
XReveranX <94445439+XReveranX@users.noreply.github.com>
2024-12-26 12:33:25 +0500

    Update second.py

--- second.py
+++ second.py
@@ -3,12 +3,30 @@
 def prv(func):
 
     def wrapper(*args):
+        if (type(*args) != type(1)) and (type(*args) != type(1.1)):
+            print("Ошибка, введите число.")
+            return
         print("Тип аргумента функции", type(*args))
         return func(*args)
     return wrapper
 
-@prv
-def func(arg):
-    print(arg)
 
-func([11,6,15,86])
+def zamican():
+    iashik = 0
+    count = 0
+    
+    @prv
+    def sredn(curr):
+        nonlocal iashik, count
+        count += 1
+        iashik += curr
+        return (iashik / count)
+    return sredn
+
+
+sbogom = zamican()
+sbogom(2.0)
+sbogom(11)
+sbogom("RaaaaR")
+sbogom(6.5)
+print(sbogom(86))
